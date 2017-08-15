cfde65147c97b892f6b04f8249abb51916c494c2
SimonDanisch <sdanisch@gmail.com>
2017-08-15 16:57:42 +0200

    commit

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,14 +9,18 @@ for file in benchmark_files
     mod = include(file * ".jl")
     @benchgroup file begin
         for T in mod.types()
-            for device in GPUBenchmarks.devices()
-                if mod.is_device_supported(device)
-                    for N in mod.nrange()
-                        println("Benchmarking $N, $T $device")
-                        # result = mod.execute(f, args)
-                        dev_quote = QuoteNode(device)
-                        b = @benchmarkable $(mod.execute)(f, args) setup = ((f, args) = $(mod.setup)($N, $T, $dev_quote)) teardown = $(mod.teardown)(args)
-                        PkgBenchmark._top_group()["$device for $T & $N"] = b
+            @benchgroup "$T" begin
+                for device in GPUBenchmarks.devices()
+                    if mod.is_device_supported(device)
+                        @benchgroup "$device" begin
+                            for N in mod.nrange()
+                                println("Benchmarking $N, $T $device")
+                                # result = mod.execute(f, args)
+                                dev_quote = QuoteNode(device)
+                                b = @benchmarkable $(mod.execute)(f, args) setup = ((f, args) = $(mod.setup)($N, $T, $dev_quote)) teardown = $(mod.teardown)(args)
+                                PkgBenchmark._top_group()[N] = b
+                            end
+                        end
                     end
                 end
             end
